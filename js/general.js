2343047d3302fa9e8931f8c4d88021df33eb0d08
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-17 22:41:15 -0500

    Update general.js

--- js/general.js
+++ js/general.js
@@ -1,1 +1,12 @@
-
+d3.select("#tech")
+.on("click", function(){
+    d3.select("#click")
+    .text("Tech Companies with Profit:10")
+         
+    
+})
+d3.select("#Nontech")
+.on("click", function(){
+    d3.select("#click")
+    .text("Non-Tech Companies with Profit:6")
+})
