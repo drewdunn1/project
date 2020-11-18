b06e640c39d670f2ff03d5e93e73558a0227cf56
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-17 22:24:45 -0500

    Update apple2009.js

--- js/apple2009.js
+++ js/apple2009.js
@@ -1,1 +1,10 @@
+var retrievePromise = d3.csv("Data/apple2009.csv");
+var successFCN = function(stock) {
+    console.log("data", stock);
+}
 
+
+var failFCN = function(errMessage) {
+    console.log("failure", errMessage);
+}
+retrievePromise.then(successFCN, failFCN);
