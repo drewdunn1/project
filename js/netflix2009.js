1ea95909d6f3bef34fd43e9817c3fdb6fc7670cf
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-17 22:25:45 -0500

    Update netflix2009.js

--- js/netflix2009.js
+++ js/netflix2009.js
@@ -1,1 +1,10 @@
+var retrievePromise = d3.csv("Data/netflix2009.csv");
+var successFCN = function(stock) {
+    console.log("data", stock);
+}
 
+
+var failFCN = function(errMessage) {
+    console.log("failure", errMessage);
+}
+retrievePromise.then(successFCN, failFCN);
